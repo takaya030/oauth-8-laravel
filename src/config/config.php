b6c89df7ed542b38b504803fcdc38e1a5d9c0844
Minoru Takayama <takaya030@yahoo.co.jp>
2021-08-01 12:26:27 +0900

    fix OAuthLaravelSession's name in config

--- src/config/config.php
+++ src/config/config.php
@@ -12,7 +12,7 @@ return [
 	 * Storage
 	 */
 	'storage' => '\\OAuth\\Common\\Storage\\Session',
-	//'storage' => '\\Takaya030\\OAuth\\OAuthLaravelnSession',
+	//'storage' => '\\Takaya030\\OAuth\\OAuthLaravelSession',
 
 	/**
 	 * Consumers
